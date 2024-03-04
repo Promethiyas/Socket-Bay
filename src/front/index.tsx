6bb33805fb352a307ff4227b0569f33a9080ee69
SpyMan10 <dev.spyman@gmail.com>
2024-03-04 11:59:06 +0100

    dev init

---
+++ src/front/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import ReactDom from 'react-dom/client'
+
+const host = document.querySelector('#root')
+if (!host) console.log('Missing react mounting point: #root')
+else {
+  const root = ReactDom.createRoot(host)
+  root.render(<App />)
+}
+
+function App() {
+  return <React.StrictMode>
+    <></>
+  </React.StrictMode>
+}
